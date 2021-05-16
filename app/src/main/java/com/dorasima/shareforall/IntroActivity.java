905e59e3b54359ef81992189fa25ed7da815c200
Dorasima-98 <cjk1516@gmail.com>
2021-05-16 11:51:34 +0900

    ~ set IntroActivity
    Todo: need to make mainActivity (with Agora, Mypage, Addition)
    
    1. AgoraFragment
    - .... siva
    2. MypageFragment
    - logout
    - service
    3. AdditionFragment
    - just mockup

---
+++ app/src/main/java/com/dorasima/shareforall/IntroActivity.java
@@ -0,0 +1,44 @@
+package com.dorasima.shareforall;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.dorasima.shareforall.ui.login.LoginActivity;
+import com.dorasima.shareforall.ui.main.MainActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+        }
+    };
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        ActionBar bar = getSupportActionBar();
+        bar.setTitle("");
+        init();
+        handler.postDelayed(runnable,1000);
+    }
+    private void init(){
+        handler = new Handler();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        handler.removeCallbacks(runnable);
+    }
+}
